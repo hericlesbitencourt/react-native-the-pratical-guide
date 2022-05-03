82b1b8f14319a2ea93970926a4481ad08027f3e1
Hericles Bitencourt <hericlesbr@gmail.com>
2022-05-03 06:34:10 -0300

    Section 01 - Lesson 15

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -1,10 +1,14 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Another piece of text!</Text>
-      <Text>Hello World!</Text>
+      <Text style={styles.dummyText}>Another piece of text!</Text>
+      <Text
+        style={styles.dummyText}
+      >
+        Hello World!
+      </Text>
       <Button title="Tap me!" />
     </View>
   );
@@ -13,8 +17,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  dummyText: { 
+    margin: 16,
+    borderWidth: 1,
+    borderColor: "red",
+    padding: 16
+  }
 });
