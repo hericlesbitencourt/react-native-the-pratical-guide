64bcd6d62fdcbf4b7fe00a91abea200abb6e7b3c
Hericles Bitencourt <hericlesbr@gmail.com>
2022-05-03 06:38:31 -0300

    Section 01 - Lesson 17

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -1,30 +1,21 @@
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.dummyText}>Another piece of text!</Text>
-      <Text
-        style={styles.dummyText}
-      >
-        Hello World!
-      </Text>
-      <Button title="Tap me!" />
+    <View style={styles.appContainer}>
+      <View>
+        <TextInput placeholder="Your course goal!" />
+        <Button title="Add Goal" />
+      </View>
+      <View>
+        <Text>List of goals...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  dummyText: { 
-    margin: 16,
-    borderWidth: 1,
-    borderColor: "red",
-    padding: 16
+  appContainer: {
+    padding: 50,
   }
 });
