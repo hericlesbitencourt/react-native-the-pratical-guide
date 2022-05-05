4bb31a535ebe6ce63cc0ce4e10e21d9ae67b68d0
Hericles Bitencourt <hericlesbr@gmail.com>
2022-05-05 17:17:17 -0300

    Section 06 - Lesson 104

--- MealsApp/screens/MealDetailScreen.js
+++ MealsApp/screens/MealDetailScreen.js
@@ -1,11 +1,32 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, StyleSheet } from "react-native";
+import React from "react";
+import { MEALS } from "../data/dummy-data";
+import MealDetails from "../components/MealDetails";
 
-export function MealDetailScreen({route}) {
-  const mealId = route.params.mealId
+export function MealDetailScreen({ route }) {
+  const mealId = route.params.mealId;
+
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
   return (
     <View>
-      <Text>MealDetailScreen {mealId}</Text>
+      <Image source={{ uri: selectedMeal.imageUrl }} />
+      <Text>{selectedMeal.title}</Text>
+      <MealDetails
+        duration={selectedMeal.duration}
+        complexity={selectedMeal.complexity}
+        affordability={selectedMeal.affordability}
+      />
+      <Text>Ingredients</Text>
+      {selectedMeal.ingredients.map((ingredient) => (
+        <Text key={ingredient}>{ingredient}</Text>
+      ))}
+      <Text>Steps</Text>
+      {selectedMeal.steps.map((step) => (
+        <Text key={step}>{step}</Text>
+      ))}
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({});
