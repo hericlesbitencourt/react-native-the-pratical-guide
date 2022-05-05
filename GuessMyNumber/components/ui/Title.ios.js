d9859fbbfe414c9948e1db8475ecc94d35bce301
Hericles Bitencourt <hericlesbr@gmail.com>
2022-05-05 08:51:49 -0300

    Section 05 - Lesson 88

---
+++ GuessMyNumber/components/ui/Title.ios.js
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, Platform } from "react-native";
+import { Colors } from "../../constants/colors";
+
+export function Title({ children }) {
+  return <Text style={styles.title}>{children}</Text>;
+}
+
+const styles = StyleSheet.create({
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 18,
+    color: Colors.white,
+    textAlign: "center",
+    // borderWidth: Platform.OS === 'android' ? 2 : 0,
+    padding: 12,
+    maxWidth: "80%",
+    minWidth: 300,
+  },
+});
