b48e624213a73c377f7e1d344e231956d8f5990d
Hericles Bitencourt <hericlesbr@gmail.com>
2022-05-05 03:40:22 -0300

    Section 04 - Lesson 62

--- GuessMyNumber/screens/GameScreen.js
+++ GuessMyNumber/screens/GameScreen.js
@@ -1,9 +1,10 @@
 import { StyleSheet, Text, View } from "react-native";
+import { Title } from "../components/Title";
 
 export function GameScreen() {
   return (
     <View style={styles.screen}>
-        <Text>Opponent's Guess</Text>
+        <Title>Opponent's Guess</Title>
         <View>
             <Text>Higher or lower?</Text>
         </View>
