8727b42ed2effaab275c3f6079181a1a563d5648
Hericles Bitencourt <hericlesbr@gmail.com>
2022-05-05 05:33:17 -0300

    Section 04 - Lesson 68

--- GuessMyNumber/components/ui/InstructionText.js
+++ GuessMyNumber/components/ui/InstructionText.js
@@ -1,8 +1,8 @@
 import { StyleSheet, Text } from "react-native";
 import { Colors } from "../../constants/colors";
 
-export function InstructionText({ children }) {
-  return <Text style={styles.instructionText}>{children}</Text>;
+export function InstructionText({ children, style }) {
+  return <Text style={[styles.instructionText, style]}>{children}</Text>;
 }
 
 const styles = StyleSheet.create({
