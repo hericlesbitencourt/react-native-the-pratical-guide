7b085033153728f84c15776d8e233233073ab0a3
Hericles Bitencourt <hericlesbr@gmail.com>
2022-05-05 02:03:03 -0300

    Section 04 - Lesson 48

--- GuessMyNumber/screens/StartGameScreen.js
+++ GuessMyNumber/screens/StartGameScreen.js
@@ -1,10 +1,12 @@
 import { TextInput, View } from "react-native";
+import { PrimaryButton } from "../components/PrimaryButton";
 
 export function StartGameScreen() {
   return (
     <View>
         <TextInput />
-
+        <PrimaryButton>Reset</PrimaryButton>
+        <PrimaryButton>Confirm</PrimaryButton>
     </View>
   )
 }
