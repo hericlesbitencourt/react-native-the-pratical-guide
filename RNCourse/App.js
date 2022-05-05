f18694774cc1c3cd1b6f34dd55b8278e100f82e4
Hericles Bitencourt <hericlesbr@gmail.com>
2022-05-04 22:17:16 -0300

    Section 01 - Lesson 25

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -1,5 +1,12 @@
 import { useState } from "react";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+} from "react-native";
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState("");
@@ -25,13 +32,13 @@ export default function App() {
         <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        {courseGoals.map((goal, key) => (
-          <View key={key} style={styles.goalItem}>
-            <Text style={styles.goalText}>
-              {goal}
-            </Text>
-          </View>
-        ))}
+        <ScrollView alwaysBounceVertical={false}>
+          {courseGoals.map((goal, key) => (
+            <View key={key} style={styles.goalItem}>
+              <Text style={styles.goalText}>{goal}</Text>
+            </View>
+          ))}
+        </ScrollView>
       </View>
     </View>
   );
@@ -70,5 +77,5 @@ const styles = StyleSheet.create({
   },
   goalText: {
     color: "white",
-  }
+  },
 });
