7094ec17b7020f3e2d653cfc9cb2fdf0bb342f4b
Hericles Bitencourt <hericlesbr@gmail.com>
2022-05-06 23:53:22 -0300

    Section 06 - Lesson 109

--- OtherNavigators/screens/UserScreen.js
+++ OtherNavigators/screens/UserScreen.js
@@ -1,12 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Button, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
-export function UserScreen() {
+export function UserScreen({ navigation }) {
+  function openDrawerHandler() {
+    navigation.toggleDrawer();
+  }
+
   return (
     <View>
       <Text>User Screen</Text>
+      <Button title="Open Drawer" onPress={openDrawerHandler} />
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
