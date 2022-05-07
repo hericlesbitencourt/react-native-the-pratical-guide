af36cdc9dfe85fd89803d4597c24bfd2ba084c88
Hericles Bitencourt <hericlesbr@gmail.com>
2022-05-07 00:00:00 -0300

    Section 06 - Lesson 110

--- OtherNavigators/App.js
+++ OtherNavigators/App.js
@@ -1,44 +1,37 @@
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { WelcomeScreen } from './screens/WelcomeScreen';
 import { UserScreen } from './screens/UserScreen';
 import { Ionicons } from '@expo/vector-icons';
 
-const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator
+      <Tab.Navigator
         screenOptions={{
           headerStyle: { backgroundColor: '#3c0a6b' },
           headerTintColor: 'white',
-          drawerActiveBackgroundColor: '#f0e1ff',
-          drawerActiveTintColor: '#3c0a6b',
-          // drawerStyle: { backgroundColor: '#ccc' },
+          tabBarActiveTintColor: '#3c0a6b',
         }}
       >
-        <Drawer.Screen
+        <Tab.Screen
           name='Welcome'
           component={WelcomeScreen}
           options={{
-            drawerLabel: 'Welcome Screen',
-            drawerIcon: ({ color, size }) => (
-              <Ionicons name='home' color={color} size={size} />
-            ),
+            tabBarIcon: ({color, size}) => <Ionicons name='home' color={color} size={size} /> 
           }}
         />
-        <Drawer.Screen
+        <Tab.Screen
           name='User'
           component={UserScreen}
           options={{
-            drawerIcon: ({ color, size }) => (
-              <Ionicons name='person' color={color} size={size} />
-            ),
+            tabBarIcon: ({color, size}) => <Ionicons name='person' color={color} size={size} />
           }}
         />
-      </Drawer.Navigator>
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
