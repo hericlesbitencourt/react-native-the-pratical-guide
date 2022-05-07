02f02686abecd3e404a2d17dd39f410435ff3b21
Hericles Bitencourt <hericlesbr@gmail.com>
2022-05-07 07:12:51 -0300

    Section 08 - Lesson 136

--- ExpenseTracker/App.js
+++ ExpenseTracker/App.js
@@ -14,19 +14,19 @@ const BottomTabs = createBottomTabNavigator();
 function ExpensesOverview() {
   return (
     <BottomTabs.Navigator
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: GlobalStyles.colors.primary500,
-        },
-        headerTintColor: 'white',
-        tabBarStyle: {
-          backgroundColor: GlobalStyles.colors.primary500,
-        },
-        tabBarActiveTintColor: GlobalStyles.colors.accent500,
-        headerRight: ({tintColor}) => {
-          return <IconButton icon='add' size={24} color={tintColor} onPress={() => {}} />
-        }
-      }}
+        screenOptions={({navigation}) =>({
+          headerStyle: {
+            backgroundColor: GlobalStyles.colors.primary500,
+          },
+          headerTintColor: 'white',
+          tabBarStyle: {
+            backgroundColor: GlobalStyles.colors.primary500,
+          },
+          tabBarActiveTintColor: GlobalStyles.colors.accent500,
+          headerRight: ({tintColor}) => {
+            return <IconButton icon='add' size={24} color={tintColor} onPress={() => navigation.navigate('ManageExpense')} />
+          }
+        })}
     >
       <BottomTabs.Screen
         name='RecentExpenses'
