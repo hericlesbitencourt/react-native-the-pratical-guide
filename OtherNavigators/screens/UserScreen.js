92b12ef8dfebe2d44899b40c3753bb4a1cdf9ca3
Hericles Bitencourt <hericlesbr@gmail.com>
2022-05-06 22:34:12 -0300

    Section 06 - Lesson 108

---
+++ OtherNavigators/screens/UserScreen.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export function UserScreen() {
+  return (
+    <View>
+      <Text>User Screen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ OtherNavigators/screens/WelcomeScreen.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export function WelcomeScreen() {
+  return (
+    <View>
+      <Text>Welcome Screen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
