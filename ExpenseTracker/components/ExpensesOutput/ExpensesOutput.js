92ab91f1ce2e545b940c608db3fb5e92e3cecf1b
Hericles Bitencourt <hericlesbr@gmail.com>
2022-05-07 05:44:10 -0300

    Section 08 - Lesson 132

--- ExpenseTracker/components/ExpensesOutput/ExpensesOutput.js
+++ ExpenseTracker/components/ExpensesOutput/ExpensesOutput.js
@@ -1,4 +1,5 @@
 import { StyleSheet, View } from 'react-native';
+import { GlobalStyles } from '../../constants/styles';
 import { ExpensesList } from './ExpensesList';
 import { ExpensesSummary } from './ExpensesSummary';
 
@@ -37,11 +38,17 @@ const DUMMY_EXPENSES = [
 
 export function ExpensesOutput({ expenses, expensesPeriod }) {
   return (
-    <View>
+    <View style={styles.container}>
       <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
       <ExpensesList expenses={DUMMY_EXPENSES} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 24,
+        backgroundColor: GlobalStyles.colors.primary700
+    }
+});
