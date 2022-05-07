7dc48ec4c82b374bc1c5be4c51a1a66109ac003f
Hericles Bitencourt <hericlesbr@gmail.com>
2022-05-07 02:42:47 -0300

    Section 07 - Lesson 116

--- MealsApp/store/context/favorites-context.js
+++ MealsApp/store/context/favorites-context.js
@@ -1,14 +1,29 @@
 import { createContext } from 'react';
 
 export const FavoritesContext = createContext({
-    ids: [],
-    addFavorite: (id) => {},
-    removeFavorite: (id) => {},
+  ids: [],
+  addFavorite: (id) => {},
+  removeFavorite: (id) => {},
 });
 
+export function FavoritesContextProvider({ children }) {
+  const [favoriteMealIds, setFavoriteMealIds] = useState([]);
 
-export function FavoritesContextProvider({children}) {
-  return (
-    <Favorites.Provider>{children}</Favorites.Provider>
-  )
+  function addFavorite(id) {
+    setFavoriteMealIds((currentFavIds) => [...currentFavIds, id]);
+  }
+
+  function removeFavorite(id) {
+    setFavoriteMealIds((currentFavIds) =>
+      currentFavIds.filter((mealId) => mealId.id !== id)
+    );
+  }
+
+  const value = {
+    ids: favoriteMealIds,
+    addFavorite: addFavorite,
+    removeFavorite: removeFavorite,
+  };
+
+  return <Favorites.Provider value={value}>{children}</Favorites.Provider>;
 }
