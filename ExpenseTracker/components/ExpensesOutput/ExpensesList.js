0584226ae14d0f9ea62d790d8c7a456da3711a1f
Hericles Bitencourt <hericlesbr@gmail.com>
2022-05-07 04:19:11 -0300

    Section 08 - Lesson 128

---
+++ ExpenseTracker/components/ExpensesOutput/ExpensesList.js
@@ -0,0 +1,9 @@
+import { FlatList, StyleSheet } from 'react-native'
+
+export function ExpensesList() {
+  return (
+    <FlatList />
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ ExpenseTracker/components/ExpensesOutput/ExpensesSummary.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export function ExpensesSummary() {
+  return (
+    <View>
+      <Text>Last 7 days</Text>
+      <Text>$177.98</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
