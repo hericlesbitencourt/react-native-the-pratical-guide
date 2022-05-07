1993e2957b90c9d8feebcdc955e19fd865e14cc7
Hericles Bitencourt <hericlesbr@gmail.com>
2022-05-07 03:50:18 -0300

    Section 08 - Lesson 125

---
+++ ExpenseTracker/screens/AllExpenses.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+ function AllExpenses() {
+  return (
+    <View>
+      <Text>AllExpenses</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ ExpenseTracker/screens/RecentExpenses.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export function RecentExpenses() {
+  return (
+    <View>
+      <Text>RecentExpenses</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
