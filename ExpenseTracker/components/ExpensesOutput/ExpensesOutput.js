31a028a74c895dfd6baffb0be19d8942416c9c2f
Hericles Bitencourt <hericlesbr@gmail.com>
2022-05-07 07:55:30 -0300

    Section 08 - Lesson 142

--- ExpenseTracker/components/ExpensesOutput/ExpensesOutput.js
+++ ExpenseTracker/components/ExpensesOutput/ExpensesOutput.js
@@ -1,46 +1,15 @@
 import { StyleSheet, View } from 'react-native';
 import { GlobalStyles } from '../../constants/styles';
+import { initialState } from '../../store/expenses-context';
 import { ExpensesList } from './ExpensesList';
 import { ExpensesSummary } from './ExpensesSummary';
 
-const DUMMY_EXPENSES = [
-    {
-        id: 'e1',
-        description: 'A pair of shoes',
-        amount: 59.99,
-        date: new Date('2021-12-19')
-    },
-    {
-        id: 'e2',
-        description: 'A pair of trousers',
-        amount: 89.99,
-        date: new Date('2022-01-06')
-    },
-    {
-        id: 'e3',
-        description: 'Some bananas',
-        amount: 6.99,
-        date: new Date('2021-12-24')
-    },
-    {
-        id: 'e4',
-        description: 'A book',
-        amount: 76.99,
-        date: new Date('2022-02-08')
-    },
-    {
-        id: 'e5',
-        description: 'Another book',
-        amount: 26.99,
-        date: new Date('2022-03-18')
-    },
-]
 
 export function ExpensesOutput({ expenses, expensesPeriod }) {
   return (
     <View style={styles.container}>
-      <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
-      <ExpensesList expenses={DUMMY_EXPENSES} />
+      <ExpensesSummary expenses={initialState} periodName={expensesPeriod} />
+      <ExpensesList expenses={initialState} />
     </View>
   );
 }
