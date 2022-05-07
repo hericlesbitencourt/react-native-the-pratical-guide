af02aa5a04bd33435ce05f1452738c97ac688789
Hericles Bitencourt <hericlesbr@gmail.com>
2022-05-07 03:38:41 -0300

    Section 07 - Lesson 122

--- MealsApp/screens/FavoritesScreen.js
+++ MealsApp/screens/FavoritesScreen.js
@@ -1,36 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useContext } from 'react'
-import { MEALS } from '../data/dummy-data'
-import { MealsList } from '../components/MealsList/MealsList'
-import { FavoritesContext } from '../store/context/favorites-context'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { MEALS } from '../data/dummy-data';
+import { MealsList } from '../components/MealsList/MealsList';
+import { useSelector } from 'react-redux';
+// import { FavoritesContext } from '../store/context/favorites-context'
 
 export function FavoritesScreen() {
-  const favoritesMealsCtx = useContext(FavoritesContext)
+  // const favoritesMealsCtx = useContext(FavoritesContext)
+  const favoriteMealsIds = useSelector((state) => state.favoriteMeals.ids);
 
-  const favoriteMeals = MEALS.filter(meal => favoritesMealsCtx.ids.includes(meal.id))
+  const favoriteMeals = MEALS.filter((meal) =>
+    favoriteMealsIds.includes(meal.id)
+  );
 
-  if(favoriteMeals.length === 0) {
+  if (favoriteMeals.length === 0) {
     return (
       <View style={styles.rootContainer}>
         <Text style={styles.text}>You have no favorites meals yet.</Text>
       </View>
-    )
+    );
   }
 
-  return (
-    <MealsList items={favoriteMeals} />
-  )
+  return <MealsList items={favoriteMeals} />;
 }
 
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   text: {
     fontSize: 18,
     fontWeight: 'bold',
-    color: 'white'
-  }
-})
+    color: 'white',
+  },
+});
