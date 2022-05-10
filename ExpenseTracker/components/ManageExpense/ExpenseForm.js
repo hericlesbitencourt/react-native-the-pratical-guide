fb3ae30e56a6bb3d4d502bed93485c2df04dbb3c
Hericles Bitencourt <hericlesbr@gmail.com>
2022-05-09 21:25:14 -0300

    Section 09 - Lesson 151

--- ExpenseTracker/components/ManageExpense/ExpenseForm.js
+++ ExpenseTracker/components/ManageExpense/ExpenseForm.js
@@ -4,22 +4,27 @@ import { Input } from './Input';
 export function ExpenseForm() {
   function amountChangeHandler() {}
   return (
-    <View>
-      <Input
-        label='Amount'
-        textInputConfig={{
-          keyboardType: 'decimal-pad',
-          onChange: amountChangeHandler,
-        }}
-      />
-      <Input
-        label='Date'
-        textInputConfig={{
-          placeholder: 'YYYY-MM-DD',
-          maxLength: 10,
-          onChange: () => {},
-        }}
-      />
+    <View style={styles.form}>
+      <Text style={styles.title}>Your Expense</Text>
+      <View style={styles.inputsRow}>
+        <Input
+          style={styles.rowInput}
+          label='Amount'
+          textInputConfig={{
+            keyboardType: 'decimal-pad',
+            onChange: amountChangeHandler,
+          }}
+        />
+        <Input
+          style={styles.rowInput}
+          label='Date'
+          textInputConfig={{
+            placeholder: 'YYYY-MM-DD',
+            maxLength: 10,
+            onChange: () => {},
+          }}
+        />
+      </View>
       <Input
         label='Description'
         textInputConfig={{
@@ -32,4 +37,22 @@ export function ExpenseForm() {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  form: {
+    marginTop: 40,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+    marginVertical: 24,
+    textAlign: 'center',
+  },
+  inputsRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  rowInput: {
+    flex: 1,
+  },
+});
