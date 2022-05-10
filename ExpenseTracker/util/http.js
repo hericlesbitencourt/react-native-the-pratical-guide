1bd7b110bab664ca6a0b87688ea3cd71b4f1def7
Hericles Bitencourt <hericlesbr@gmail.com>
2022-05-09 23:05:37 -0300

    Section 10 - Lesson 164

--- ExpenseTracker/util/http.js
+++ ExpenseTracker/util/http.js
@@ -1,8 +1,26 @@
 import axios from 'axios';
 
 const BASE_URL =
-  'https://react-native-course-51e2e-default-rtdb.firebaseio.com/';
+  'https://react-native-course-51e2e-default-rtdb.firebaseio.com';
 
 export function storeExpense(expenseData) {
-  axios.post(BASE_URL + 'expenses.json', expenseData);
+  axios.post(BASE_URL + '/expenses.json', expenseData);
+}
+
+export async function fetchExpenses() {
+  const response = await axios.get(BASE_URL + '/expenses.json');
+
+  const expenses = [];
+
+  for (const key in response.data) {
+    const expenseObj = {
+      id: key,
+      amount: response.data[key].amount,
+      date: new Date(response.data[key].date),
+      description: response.data[key].description,
+    };
+    expenses.push(expenseObj);
+  }
+
+  return expenses;
 }
