2c2d4b7eb49d7b233212d7bbf57e757a8052bd65
Hericles Bitencourt <hericlesbr@gmail.com>
2022-05-10 00:43:17 -0300

    Section 11 - Lesson 179

--- UserAuthentication/screens/SignupScreen.js
+++ UserAuthentication/screens/SignupScreen.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Alert } from 'react-native';
 import AuthContent from '../components/Auth/AuthContent';
 import LoadingOverlay from '../components/ui/LoadingOverlay';
 import { createUser } from '../util/auth';
@@ -7,7 +8,11 @@ function SignupScreen() {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
   async function signupHandler({ email, password }) {
     setIsAuthenticating(true);
-    await createUser(email, password);
+    try {
+      await createUser(email, password);
+    } catch (error) {
+      Alert.alert('Authentication failed!', 'Could not create user, please check your input and try again later.')
+    }
     setIsAuthenticating(false);
   }
 
