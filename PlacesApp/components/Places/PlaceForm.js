d3a2bd7d80440989e526f9b5edc5c41d13d828e6
Hericles Bitencourt <hericlesbr@gmail.com>
2022-05-24 13:10:50 -0300

    Section 12 - Lesson 200

--- PlacesApp/components/Places/PlaceForm.js
+++ PlacesApp/components/Places/PlaceForm.js
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import { ScrollView, StyleSheet, Text, TextInput, View } from 'react-native'
-import { Colors } from '../../constants/colors'
-import ImagePicker from './ImagePicker'
+import { useState } from 'react';
+import { ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Colors } from '../../constants/colors';
+import ImagePicker from './ImagePicker';
+import LocationPicker from './LocationPicker';
 
 export default function PlaceForm() {
-  const [enteredTitle, setEnteredTitle] = useState('')
+  const [enteredTitle, setEnteredTitle] = useState('');
 
   function changeTitleHander(enteredText) {
-    setEnteredTitle(enteredText)
+    setEnteredTitle(enteredText);
   }
 
   return (
     <ScrollView style={styles.form}>
       <View>
         <Text style={styles.label}>Title</Text>
-        <TextInput style={styles.input} onChangeText={changeTitleHander} value={enteredTitle} />
+        <TextInput
+          style={styles.input}
+          onChangeText={changeTitleHander}
+          value={enteredTitle}
+        />
       </View>
       <ImagePicker />
+      <LocationPicker />
     </ScrollView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   form: {
     flex: 1,
-    padding: 24
+    padding: 24,
   },
   label: {
     fontWeight: 'bold',
     marginBottom: 4,
-    color: Colors.primary500
+    color: Colors.primary500,
   },
   input: {
     marginVertical: 8,
@@ -38,7 +44,6 @@ const styles = StyleSheet.create({
     fontSize: 16,
     borderBottomColor: Colors.primary700,
     borderBottomWidth: 2,
-    backgroundColor: Colors.primary100
-
+    backgroundColor: Colors.primary100,
   },
-})
+});
