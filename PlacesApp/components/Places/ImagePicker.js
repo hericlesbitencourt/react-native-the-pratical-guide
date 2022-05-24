1d99e937cdbe8006286838594e80255c10d60843
Hericles Bitencourt <hericlesbr@gmail.com>
2022-05-24 12:30:04 -0300

    Section 12 - Lesson 195

---
+++ PlacesApp/components/Places/ImagePicker.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function ImagePicker() {
+  return (
+    <View>
+      <Text>ImagePicker</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
