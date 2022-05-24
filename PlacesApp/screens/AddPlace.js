cb4ed08590048e7d8d8bb74f30d11b6bb508b3ff
Hericles Bitencourt <hericlesbr@gmail.com>
2022-05-24 10:16:10 -0300

    Section 12 - Lesson 191

---
+++ PlacesApp/screens/AddPlace.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native'
+import PlaceForm from '../components/Places/PlaceForm'
+
+export default function AddPlace() {
+  return (
+    <PlaceForm />
+  )
+}
+
+const styles = StyleSheet.create({})
