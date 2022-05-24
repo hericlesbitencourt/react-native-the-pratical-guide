3fbf1eba0d537111db07eb75902227c035c1852c
Hericles Bitencourt <hericlesbr@gmail.com>
2022-05-24 15:48:57 -0300

    Section 12 - Lesson 211

--- PlacesApp/screens/AllPlaces.js
+++ PlacesApp/screens/AllPlaces.js
@@ -1,8 +1,18 @@
+import { useIsFocused } from '@react-navigation/native';
+import { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import PlacesList from '../components/Places/PlacesList';
 
-export default function AllPlaces() {
-  return <PlacesList />;
+export default function AllPlaces({ route }) {
+  const [loadedPlaces, setLoadedPlaces] = useState([]);
+  const isFocused = useIsFocused();
+  useEffect(() => {
+    if (isFocused && route.params) {
+      setLoadedPlaces(currentPlaces => [...currentPlaces, route.params.place]);
+    }
+  }, [isFocused, route]);
+
+  return <PlacesList places={loadedPlaces} />;
 }
 
 const styles = StyleSheet.create({});
