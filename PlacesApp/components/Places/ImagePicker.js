e3a710dc6fb5f98a06121d784c9724ed453e2f40
Hericles Bitencourt <hericlesbr@gmail.com>
2022-05-24 12:42:03 -0300

    Section 12 - Lesson 197

--- PlacesApp/components/Places/ImagePicker.js
+++ PlacesApp/components/Places/ImagePicker.js
@@ -1,8 +1,40 @@
-import { Button, StyleSheet, View } from 'react-native';
-import { launchCameraAsync } from 'expo-image-picker';
+import { Alert, Button, StyleSheet, View } from 'react-native';
+import {
+  launchCameraAsync,
+  useCameraPermissions,
+  PermissionStatus,
+} from 'expo-image-picker';
 
 export default function ImagePicker() {
+  const [cameraPermissionInformation, requestPermission] =
+    useCameraPermissions();
+
+  async function verifyPermissions() {
+    if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+      const permisionResponse = await requestPermission();
+
+      return permisionResponse.granted;
+    }
+
+    if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        'Insuficient Permissions!',
+        'You need to grant camera permissions to use this app.'
+      );
+
+      return false;
+    }
+
+    return true;
+  }
+
   async function takeImageHandler() {
+    const hasPermission = await verifyPermissions();
+
+    if (!hasPermission) {
+      return;
+    }
+
     const image = await launchCameraAsync({
       allowsEditing: true,
       aspect: [16, 9],
