0e18c503f8931e61dc6fc410e27406d755c82156
Hericles Bitencourt <hericlesbr@gmail.com>
2022-05-24 15:00:35 -0300

    Section 12 - Lesson 204

--- PlacesApp/screens/Map.js
+++ PlacesApp/screens/Map.js
@@ -1,18 +1,43 @@
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 
 export default function Map() {
-    const region = {
-        latitude: 37.78,
-        longitude: -122.43,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-    };
-  return <MapView style={styles.map} initialRegion={region}></MapView>;
+  const [selectedLocation, setSelectedLocation] = useState();
+  const region = {
+    latitude: 37.78,
+    longitude: -122.43,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  };
+
+  function selectLocationHandler(event) {
+    const latitude = event.nativeEvent.coordinate.latitude;
+    const longitude = event.nativeEvent.coordinate.longitude;
+    setSelectedLocation({ latitude: latitude, longitude: longitude });
+  }
+
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={region}
+      onPress={selectLocationHandler}
+    >
+      {selectedLocation && (
+        <Marker
+          title='Picked Location'
+          coordinate={{
+            latitude: selectedLocation.latitude,
+            longitude: selectedLocation.longitude,
+          }}
+        />
+      )}
+    </MapView>
+  );
 }
 
 const styles = StyleSheet.create({
-    map: {
-        flex: 1,
-    }
+  map: {
+    flex: 1,
+  },
 });
