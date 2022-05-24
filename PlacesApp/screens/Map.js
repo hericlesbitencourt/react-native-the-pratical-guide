9144252849d0947936580842471e73ca5b3aafde
Hericles Bitencourt <hericlesbr@gmail.com>
2022-05-24 15:07:52 -0300

    Section 12 - Lesson 205

--- PlacesApp/screens/Map.js
+++ PlacesApp/screens/Map.js
@@ -1,8 +1,9 @@
-import { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useCallback, useLayoutEffect, useState } from 'react';
+import { Alert, StyleSheet } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
+import IconButton from '../components/UI/IconButton';
 
-export default function Map() {
+export default function Map({ navigation }) {
   const [selectedLocation, setSelectedLocation] = useState();
   const region = {
     latitude: 37.78,
@@ -16,6 +17,37 @@ export default function Map() {
     const longitude = event.nativeEvent.coordinate.longitude;
     setSelectedLocation({ latitude: latitude, longitude: longitude });
   }
+
+  const savePickedLocationHandler = useCallback(() => {
+    if (!selectedLocation) {
+      Alert.alert(
+        'No location picked!',
+        'You have to pick a location (by tapping on the map) first!'
+      );
+      return;
+    }
+
+    navigation.navigate('AddPlace', {
+      pickedLatitude: selectedLocation.latitude,
+      pickedLongitude: selectedLocation.longitude,
+    });
+  }, [navigation, selectedLocation]);
+
+  useLayoutEffect(() => {
+    navigation.setOptions(
+      {
+        headerRight: ({ tintColor }) => (
+          <IconButton
+            icon='save'
+            size={24}
+            color={tintColor}
+            onPress={savePickedLocationHandler}
+          />
+        ),
+      },
+      [navigation, savePickedLocationHandler]
+    );
+  });
 
   return (
     <MapView
