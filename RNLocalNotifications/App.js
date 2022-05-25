5f55de48591bc4bc5b49271637a24edd59575c2e
Hericles Bitencourt <hericlesbr@gmail.com>
2022-05-25 17:02:12 -0300

    Section 15 - Lesson 249

--- RNLocalNotifications/App.js
+++ RNLocalNotifications/App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, View } from 'react-native';
 import * as Notifications from 'expo-notifications';
+import { useEffect } from 'react';
 
 Notifications.setNotificationHandler({
   handleNotification: async () => {
@@ -13,6 +14,20 @@ Notifications.setNotificationHandler({
 });
 
 export default function App() {
+  useEffect(() => {
+    const subscription = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        console.log('NOTIFICATION RECEIVED');
+        console.log(notification);
+        const userName = notification.request.content.data.userName
+        console.log(userName);
+      }
+    );
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+
   function scheduleNotificationHandler() {
     Notifications.scheduleNotificationAsync({
       content: {
