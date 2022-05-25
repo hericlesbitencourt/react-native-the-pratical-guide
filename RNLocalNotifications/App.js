3172478b93722179704bf35cbe632c85cdd3bc12
Hericles Bitencourt <hericlesbr@gmail.com>
2022-05-25 12:53:47 -0300

    Section 15 - Lesson 245

--- RNLocalNotifications/App.js
+++ RNLocalNotifications/App.js
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import * as Notifications from 'expo-notifications';
 
 export default function App() {
+  function scheduleNotificationHandler() {
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'My first local notification',
+        body: 'This is the body of the notification',
+        data: { userName: 'Hericles' },
+      },
+      trigger: {
+        seconds: 5,
+      },
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button
+        title='Schedule Notification'
+        onPress={scheduleNotificationHandler}
+      />
+      <StatusBar style='auto' />
     </View>
   );
 }
